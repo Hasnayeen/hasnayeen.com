0fe52587473103f8c169963bd05018c92a728a11
Hasnayeen <searching.nehal@gmail.com>
2025-01-19 12:04:52 +0600

    add umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hasnayeen.com",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    mdx(),
+    sitemap(),
+    solidJs(),
+    tailwind({ applyBaseStyles: false }),
+    umami({
+      id: "1561df43-ed2b-45c1-8f4c-d35b83b0059b",
+    })
+  ],
 })
